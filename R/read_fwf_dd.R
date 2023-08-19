a9b31967e3ae518735c499659bd8d05f5dca0866
Travis Gerke <tgerke@mail.harvard.edu>
2023-08-18 18:42:50 -0700

    establish framework for labelling factors

--- R/read_fwf_dd.R
+++ R/read_fwf_dd.R
@@ -12,13 +12,35 @@
 #' fixer:::read_fwf_example("example-fwf-data.dat") |>
 #'   read_fwf_dd(dd)
 read_fwf_dd <- function(path_fwf, dd) {
+
   fwf <- readr::read_fwf(
     file = path_fwf,
     col_positions = readr::fwf_widths(dd$col_width, dd$var_name)
   )
 
+  # set variable labels
   out <- fwf %>%
     labelled::set_variable_labels(.labels = dd$var_label)
+
+  # set factor labels when relevant
+
+  # this base R filter is needed because dplyr::filter doesn't like list cols
+  labelled_factors <- dd[vapply(dd$fct_labels, function(x) !is.null(x), TRUE),]
+
+  # make a named vector for each factor and its labels
+  vec_names <- labelled_factors[1, "fct_labels"] %>%
+    unlist() %>%
+    purrr::map_chr(~ strsplit(.x, ". ", fixed = TRUE)[[1]][2])
+
+  fct_labels <- labelled_factors[1, "fct_labels"] %>%
+    unlist() %>%
+    purrr::map_dbl(~ strsplit(.x, ". ", fixed = TRUE)[[1]][1] %>% as.double()) %>%
+    rlang::set_names(vec_names)
+
+  out %>%
+    labelled::set_value_labels(
+      racebld = fct_labels
+    )
 
   return(out)
 }
